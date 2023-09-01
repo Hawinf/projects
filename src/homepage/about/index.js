92183fd0b77c98cadc6964b217c14afc407cda04
Hawinf <hawinf54@gmail.com>
2023-09-01 22:38:24 +0400

    x

--- src/homepage/about/index.js
+++ src/homepage/about/index.js
@@ -11,20 +11,22 @@ const About = () => {
             </div>
             <div className="about-content">
                 <div className="container">
-                    <div className="row">
-                            <div className="col-lg-6 col-md-6 col-sm-6">
-                                <div className="content-left">
-                                    <img className="about-pict" src={ABOUTPIC} />
-                                    {/* <h6>DISINI BACKGROUND BERAS BERAS DISINI BACKGROUND BERAS BERAS</h6>
-                                    <h6>DISINI BACKGROUND BERAS BERAS DISINI BACKGROUND BERAS BERAS</h6> */}
+                    {/* <div className="row"> */}
+                            <div className="putar">
+                                <div className="col-lg-6 col-md-6 col-sm-6">
+                                    <div className="content-left">
+                                        <img className="about-pict" src={ABOUTPIC} />
+                                        {/* <h6>DISINI BACKGROUND BERAS BERAS DISINI BACKGROUND BERAS BERAS</h6>
+                                        <h6>DISINI BACKGROUND BERAS BERAS DISINI BACKGROUND BERAS BERAS</h6> */}
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 col-md-6 col-sm-6">
+                                    <div className="content-right">
+                                        <h4 className="content-desc">We provide quality and expert information to help konjac lovers make educated choices about specialty konjac products for dietary needs.</h4>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="col-lg-6 col-md-6 col-sm-6">
-                                <div className="content-right">
-                                    <h4 className="content-desc">We provide quality and expert information to help konjac lovers make educated choices about specialty konjac products for dietary needs.</h4>
-                                </div>
-                            </div>
-                    </div>
+                    {/* </div> */}
                 </div>
             </div>
         </div>
