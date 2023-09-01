fd8355301d01bf75e2b9ae41f572f893af0f51eb
Hawinf <hawinf54@gmail.com>
2023-09-01 21:21:37 +0400

    adding category

---
+++ src/homepage/category/category.js
@@ -0,0 +1,48 @@
+import React from "react";
+import './category.css';
+import ONE from '../../assests/number-1.png';
+import TWO from '../../assests/number-2.png'
+import THREE from '../../assests/number-3.png';
+import { Link } from "react-router-dom";
+
+const Category = () => {
+    return (
+        
+        <div className="category">
+            <h1 className="judul">Category</h1>
+            <div className="macam-macam">
+                <div  className="container">
+                    <div className="row">
+                        <div className="col-lg-4 col-md-4 col-sm-4">
+                            <div className="number">
+                                <a href="/">RICE</a>
+                                <img src={TWO} />
+                            </div>
+                        </div>
+                        <div className="col-lg-4  col-md-4  col-sm-4">
+                            <div className="number">
+                                <a  href="/">SUGAR</a>
+                                <img src={ONE} />
+                            </div>
+                        </div>
+                        <div className="col-lg-4  col-md-4  col-sm-4">
+                            <div  className="number">
+                                <a  href="/">POWDER</a>
+                                <img src={THREE} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
+    );
+};
+
+export default Category;
+
+
+ 
+                
+                 
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavbarComponent from "./navbar/navbar";
 import About from "./about";
 import Content from "./contents/content";
+import Category from "./category/category";
 
 const Homepage = () => {
     return (
@@ -9,6 +10,7 @@ const Homepage = () => {
             <NavbarComponent />
             <About />
             <Content />
+            <Category  />
         </>
     );
 };
