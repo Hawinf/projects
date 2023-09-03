e5754a56a792e6a39c3baf310aa48f00e6151cd4
Hawinf <hawinf54@gmail.com>
2023-09-04 00:13:44 +0400

    x

--- src/page/product/rice/Rice.js
+++ src/page/product/rice/Rice.js
@@ -14,11 +14,11 @@ const Rice = () => {
                         <div className="product-rice mb-5">
                             <div className="rice-left">
                                 <div className="rice-card">
-                                    <img className="detail-rice" src={RiceOrdinary}  />
+                                    <img className="detail-rice" src={RicePremium}  />
                                 </div>
                             </div>
                             <div className="rice-right">
-                                <h6 className="rice-right-judul">Rice Ordinary</h6>
+                                <h6 className="rice-right-judul">Rice Premium</h6>
                                 <p className="rice-right-desc">Treehouse California Almonds is a leader in creating diced almonds for its customers in a variety of sizes to ensure that their products retain their distinctiveness – and nutritional halo – in the marketplace. We dice natural, roasted and organic almonds for customers around the world.
                                     Food producers appreciate the precise, consistent sizes of our diced almonds, which lets them use 100% of what they receive from us – with zero waste and zero compromise in product quality or process efficiency
                                 </p>
@@ -31,7 +31,7 @@ const Rice = () => {
                         <div className="product-rice">
                             <div className="rice-left">
                                     <div className="rice-card">
-                                        <img className="detail-rice" src={RicePremium}  />
+                                        <img className="detail-rice" src={RiceOrdinary}  />
                                     </div>
                                 </div>
                                 <div className="rice-right">
