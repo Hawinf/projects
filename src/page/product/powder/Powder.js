c63668a6e616f46f6cba446377e52fcf22d29871
Hawinf <hawinf54@gmail.com>
2023-09-04 23:25:58 +0400

    link all the page

--- src/page/product/powder/Powder.js
+++ src/page/product/powder/Powder.js
@@ -16,7 +16,7 @@ const Powder = () => {
                     <div className="powder-right">
                         <h6 className="powder-right-judul">Konjac Poweder</h6>
                         <p className="powder-right-desc">Treehouse California Almonds is a leader in creating diced almonds for its customers in a variety of sizes to ensure that their products retain their distinctiveness – and nutritional halo – in the marketplace. We dice natural, roasted and organic almonds for customers around the world. Food producers appreciate the precise, consistent sizes of our diced almonds, which lets them use 100% of what they receive from us – with zero waste and zero compromise in product quality or process efficiency</p>
-                        <Link className='btn btn-success'>
+                        <Link to='/contact' className='btn btn-success'>
                             Get Quotation
                         </Link>
                     </div>
@@ -28,7 +28,7 @@ const Powder = () => {
                     <div className="powder-right">
                         <h6 className="powder-right-judul">Konjac Poweder</h6>
                         <p className="powder-right-desc">Treehouse California Almonds is a leader in creating diced almonds for its customers in a variety of sizes to ensure that their products retain their distinctiveness – and nutritional halo – in the marketplace. We dice natural, roasted and organic almonds for customers around the world. Food producers appreciate the precise, consistent sizes of our diced almonds, which lets them use 100% of what they receive from us – with zero waste and zero compromise in product quality or process efficiency</p>
-                        <Link className='btn btn-success'>
+                        <Link to='/contact' className='btn btn-success'>
                             Get Quotation
                         </Link>
                     </div>
@@ -40,7 +40,7 @@ const Powder = () => {
                     <div className="powder-right">
                         <h6 className="powder-right-judul">Konjac Poweder</h6>
                         <p className="powder-right-desc">Treehouse California Almonds is a leader in creating diced almonds for its customers in a variety of sizes to ensure that their products retain their distinctiveness – and nutritional halo – in the marketplace. We dice natural, roasted and organic almonds for customers around the world. Food producers appreciate the precise, consistent sizes of our diced almonds, which lets them use 100% of what they receive from us – with zero waste and zero compromise in product quality or process efficiency</p>
-                        <Link className='btn btn-success'>
+                        <Link to='/contact' className='btn btn-success'>
                             Get Quotation
                         </Link>
                     </div>
--- src/page/product/rice/Rice.js
+++ src/page/product/rice/Rice.js
@@ -22,7 +22,7 @@ const Rice = () => {
                                 <p className="rice-right-desc">Treehouse California Almonds is a leader in creating diced almonds for its customers in a variety of sizes to ensure that their products retain their distinctiveness – and nutritional halo – in the marketplace. We dice natural, roasted and organic almonds for customers around the world.
                                     Food producers appreciate the precise, consistent sizes of our diced almonds, which lets them use 100% of what they receive from us – with zero waste and zero compromise in product quality or process efficiency
                                 </p>
-                                <Link className="btn btn-success">
+                                <Link to='/contact' className="btn btn-success">
                                     Get Quotation
                                 </Link>
                             </div>
@@ -39,7 +39,7 @@ const Rice = () => {
                                     <p  className="rice-right-desc">Treehouse California Almonds is a leader in creating diced almonds for its customers in a variety of sizes to ensure that their products retain their distinctiveness – and nutritional halo – in the marketplace. We dice natural, roasted and organic almonds for customers around the world.
                                         Food producers appreciate the precise, consistent sizes of our diced almonds, which lets them use 100% of what they receive from us – with zero waste and zero compromise in product quality or process efficiency
                                     </p>
-                                    <Link className="btn btn-success">
+                                    <Link to='/contact' className="btn btn-success">
                                         Get Quotation
                                     </Link>
                                 </div>
