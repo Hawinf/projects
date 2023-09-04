5ca41873989a8c671f85d29b8c60240e285b177b
Hawinf <hawinf54@gmail.com>
2023-09-04 23:22:47 +0400

    borderradius in image career page

--- src/page/discover/Discover.js
+++ src/page/discover/Discover.js
@@ -16,7 +16,7 @@ const Discover = () => {
                             <div className="discover-section-left">
                                 <h2 className="discover-judul">Success Stories</h2>
                                 <p className="discover-desc">With Pricemoov by your side, it's easier than ever to optimize prices, improve margins, and negotiate deals.</p>
-                                <Link className="btn btn-success link">
+                                <Link to='/contact' className="btn btn-success link">
                                     Connect With Us
                                 </Link>
                             </div>
