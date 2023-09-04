17007450c5ac0919c10a0a41743f5434c99181fb
Hawinf <hawinf54@gmail.com>
2023-09-04 22:12:39 +0400

    fixing career

--- src/page/career/Career.js
+++ src/page/career/Career.js
@@ -6,10 +6,10 @@ import './career.css';
 import Footer from "../../homepage/footer/footer";
 
 const Career = () => {
-    const [career, setCareer] = useState(false);
+    const [career, setCareer] = useState(true);
 
     const onCareer = () => {
-        setCareer(true)
+        setCareer(false)
     }
 
     return (
@@ -19,19 +19,30 @@ const Career = () => {
                 <p className="career-desc">We’re leading the pricing revolution and we need passionate people to join the movement.</p>
                 <button className="btn btn-success tombol-career mb-5" onClick={onCareer}>Available Position</button>
                 
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-4 col-md-12 mb-2">
-                            <img className="career-image" src={CareerImg} />
+                {
+                    career ? (
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-lg-4 col-md-12 mb-2">
+                                    <img className="career-image" src={CareerImg} />
+                                </div>
+                                <div className="col-lg-4  col-md-12 mb-2">
+                                    <img className="career-image" src={CareerImg1} />
+                                </div>
+                                <div className="col-lg-4  col-md-12 mb-2">
+                                    <img className="career-image" src={CareerImg2} />
+                                </div>
+                            </div>
                         </div>
-                        <div className="col-lg-4  col-md-12 mb-2">
-                            <img className="career-image" src={CareerImg1} />
+                    ) : (
+                        <div className="container">
+                            <div className="card-career">
+                                <h4>Job Not Available</h4>
+                            </div>
                         </div>
-                        <div className="col-lg-4  col-md-12 mb-2">
-                            <img className="career-image" src={CareerImg2} />
-                        </div>
-                    </div>
-                </div>
+                    )
+                }
+                
             </div>
             <Footer />
         </>
