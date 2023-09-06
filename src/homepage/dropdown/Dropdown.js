c74db857fe5fce128af366c08099eb5e9510d04d
Hawinf <hawinf54@gmail.com>
2023-09-06 15:41:06 +0400

    adding accordion

--- src/homepage/dropdown/Dropdown.js
+++ src/homepage/dropdown/Dropdown.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Accordion from 'react-bootstrap/Accordion';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './dropdown.css';
 
 const ContentDropdown = () => {
@@ -8,14 +12,35 @@ const ContentDropdown = () => {
                 <div className="row">
                     <div className="col-lg-6">
                         <div className="drop-left">
-                            <h3>Frequently Asked Question</h3>
-                            <p>Frequently Asked Question</p>
+                            <h3 className="drop-judul">Frequently Asked Question</h3>
+                            <p className="drop-desc">Frequently Asked Question</p>
                         </div>
                     </div>
                     <div className="col-lg-6">
                         <div className="drop-right">
-                            <h3>Frequently Asked Question</h3>
-                            <p>Frequently Asked Question</p>
+                        <>
+                                <Row>
+                                    <Col>
+                                        
+                                        <div>
+                                            <Accordion defaultActiveKey="0">
+                                                <Accordion.Item eventKey="0">
+                                                    <Accordion.Header className="accor-head">Accordion Item #1</Accordion.Header>
+                                                    <Accordion.Body>
+                                                        First Item
+                                                    </Accordion.Body>
+                                                </Accordion.Item>
+                                                <Accordion.Item eventKey="1">
+                                                    <Accordion.Header>Accordion Item #2</Accordion.Header>
+                                                    <Accordion.Body>
+                                                        Second Item
+                                                    </Accordion.Body>
+                                                </Accordion.Item>
+                                            </Accordion>
+                                        </div>
+                                    </Col>
+                                </Row>
+                        </>
                         </div>
                     </div>
                     </div>
