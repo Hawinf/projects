b6b24269294c712743470571716cc6a654b049f7
Hawinf <hawinf54@gmail.com>
2023-09-13 22:35:04 +0400

    compres picture all

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
+
 import Homepage from './homepage';
 import Product from './page/product/product';
 import Rice from './page/product/rice/Rice';
@@ -14,14 +15,15 @@ import Contact from './page/contact/Contact';
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Homepage />} />
-      <Route path='/products' element={<Product />} />
-      <Route path='/rice' element={<Rice />} />
-      <Route path='/sugar' element={<Sugar />} />
-      <Route path='/powder' element={<Powder />} />
-      <Route path='/discover' element={<Discover />} />
-      <Route path='/career' element={<Career />} />
-      <Route path='/contact' element={<Contact />} />
+      
+        <Route path='/' element={<Homepage />} />
+        <Route path='/products' element={<Product />} />
+        <Route path='/rice' element={<Rice />} />
+        <Route path='/sugar' element={<Sugar />} />
+        <Route path='/powder' element={<Powder />} />
+        <Route path='/discover' element={<Discover />} />
+        <Route path='/career' element={<Career />} />
+        <Route path='/contact' element={<Contact />} />
       
     </Routes>
   );
