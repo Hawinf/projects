5d88feeb7bb3f1ee35d043a415aa5a2bc198e42d
Hawinf <hawinf54@gmail.com>
2023-09-24 22:09:15 +0400

    adding logo in category

--- src/landingpage/cate/CategoryLanding.js
+++ src/landingpage/cate/CategoryLanding.js
@@ -2,6 +2,7 @@ import React from "react";
 import Food from '../../assests/food1.jpg';
 import Furniture from '../../assests/furniture.jpg';
 import Footer from "../../homepage/footer/footer";
+import Logo from '../../assests/landsnatures-low-resolution-logo-black-on-transparent-background.png';
 import './catelanding.css';
 import { Link } from "react-router-dom";
 
@@ -10,7 +11,12 @@ const CategoryLanding = () => {
         <>
             <div className="cate-landing">
                 <div className="container">
-                    <h1 className="catelanding-judul">Category Product</h1>
+                    <div className="catenavbar">
+                        <h1 className="catelanding-judul">Category Product</h1>
+                        <Link to='/'>
+                            <img className="cate-logo" src={Logo} />
+                        </Link>
+                    </div>
                     <div className="row">
                         <div className="col-lg-6 col-md-12">
                             <div className="cate-satu">
