855cc82702a34ef87e0a4321215c21b6776e03bc
Hawinf <hawinf54@gmail.com>
2023-09-29 00:58:10 +0400

    adding dining tables

--- src/furniture/kitchen/Kitchen.js
+++ src/furniture/kitchen/Kitchen.js
@@ -3,6 +3,14 @@ import '../bedroom/bedroom.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css'
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import Footer from "../../homepage/footer/footer";
+import Kc1 from '../../assests/diningtables/Diningtable01.jpg';
+import Kc2 from '../../assests/diningtables/Diningtable02.jpg';
+import Kc3 from '../../assests/diningtables/Diningtable03.jpg';
+import Kc4 from '../../assests/diningtables/Diningtable04.jpg';
+import FurnitureHome from '../../assests/furniturehomepage.jpg';
+
 
 const Kitchen = () => {
     useEffect(() => {
@@ -10,9 +18,92 @@ const Kitchen = () => {
     },[])
 
     return (
-        <div>
-            <h5>YoYok please share the Pictures urgent hahah</h5>
-        </div>
+        <>
+            <div className="bedroom">
+                <div>
+                    <Link to='/'>
+                        <img className="bedroomlogo" src={FurnitureHome} data-aos='fade-down'/>
+                    </Link>
+                    <div className="container">
+                        <div className="bedroom-gallery">
+                            <div className="row">
+                                <div className="col-lg-3 mb-4" data-aos='fade-right'>
+                                    <h5 className="bedroom-judul">Kitchen Collection</h5>
+                                    <p>Everything in kitchen collection</p>
+                                    <Link to='/contact' className='btn btn-success kontak-bedroom'>
+                                        Get Quotation
+                                    </Link>
+                                    <h5 className="bedroom-judul mt-4">
+                                        See More Other Products
+                                    </h5>
+                                    <div>
+                                        <Link to='/homepagefurniture'>
+                                            Everything In Furniture
+                                        </Link>
+                                    </div>
+                                    <div>
+                                        <Link to='/livingroom-collections'>
+                                            Living Room Collection
+                                        </Link>
+                                    </div>
+                                    <div>
+                                        <Link to='/wardobe-collections'>
+                                            Wardrobe Collection
+                                        </Link>
+                                    </div>
+                                    <div>
+                                        <Link to='/statue-collections'>
+                                            Statue Collection
+                                        </Link>
+                                    </div>
+                                    <div>
+                                        <Link to='/bedroom-collections'>
+                                            Bedroom Collection
+                                        </Link>
+                                    </div>
+                                    <div>
+                                        <Link to='/chairs-collection'>
+                                            Chair Collection
+                                        </Link>
+                                    </div>
+                                    <div>
+                                        <Link to='/tables-collections'>
+                                            Tables Collection
+                                        </Link>
+                                    </div>
+                                </div>
+                                <div className="col-lg-9" data-aos='fade-left'>
+                                    <div className="row">
+                                        <div className="col-lg-6">
+                                            <div className="bedroom-collections">
+                                                <img className="bedroomsatu" src={Kc1} />
+                                            </div>
+                                        </div>
+                                        <div className="col-lg-6">
+                                            <div className="bedroom-collections">
+                                                <img className="bedroomsatu" src={Kc2} />
+                                            </div>
+                                        </div>
+                                        <div className="col-lg-6">
+                                            <div className="bedroom-collections">
+                                                <img className="bedroomsatu" src={Kc3} />
+                                            </div>
+                                        </div>
+                                        <div className="col-lg-6">
+                                            <div className="bedroom-collections">
+                                                <img className="bedroomsatu" src={Kc4} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <Footer />
+        </>
     );
 };
 
