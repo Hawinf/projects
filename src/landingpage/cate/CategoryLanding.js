2b22b375d792e3d97bfa71420b860e8e34fb020e
Hawinf <hawinf54@gmail.com>
2023-10-01 21:34:38 +0400

    fixing cate page

--- src/landingpage/cate/CategoryLanding.js
+++ src/landingpage/cate/CategoryLanding.js
@@ -5,23 +5,30 @@ import Footer from "../../homepage/footer/footer";
 import Logo from '../../assests/landsnatures-low-resolution-logo-black-on-transparent-background.png';
 import './catelanding.css';
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css'
 
 const CategoryLanding = () => {
+    useEffect(() => {
+        AOS.init({duration: 2000})
+    },[])
+
     return (
         <>
             <div className="cate-landing">
                 <div className="container">
                     <div className="catenavbar">
-                        <h1 className="catelanding-judul">Category Product</h1>
-                        <Link to='/'>
+                        <h1 className="catelanding-judul" data-aos='fade-right'>Category Product</h1>
+                        <Link to='/' data-aos='fade-left'>
                             <img className="cate-logo" src={Logo} />
                         </Link>
                     </div>
-                    <div className="cate-desc">
+                    <div className="cate-desc" data-aos='fade-right'>
                         <p className="desc-cate-p">Welcome to our product category, where you'll find a diverse selection of high-quality items to cater to your needs and desires. We take pride in offering a wide range of products that cater to various tastes, preferences, and lifestyles, ensuring that you can find what you're looking for.</p>
                         <p className="desc-cate-p">Experience the convenience of online shopping with our product category. Whether you're searching for essentials or something unique, we've got you covered. Start exploring our range today!</p>
                     </div>
-                    <div className="row mt-5">
+                    <div className="row mt-5" data-aos='fade-right'>
                         <div className="col-lg-6 col-md-12">
                             <div className="cate-satu">
                                 <Link to='/homepagefurniture'>
@@ -29,7 +36,7 @@ const CategoryLanding = () => {
                                 </Link>
                             </div>
                         </div>
-                        <div className="col-lg-6 col-md-12">
+                        <div className="col-lg-6 col-md-12" data-aos='fade-left'>
                             <div className="cate-satu">
                                 <Link to='/homepagefood'>
                                     <img className="cate-foto" src={Food} />
